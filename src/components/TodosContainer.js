2384da2e6643e3f018101176e6daa161bd3d1d5b
POOJAKGUPTA <POOJAKGUPTA4@GMAIL.com>
2019-10-05 18:03:43 +0530

    new theme

--- src/components/TodosContainer.js
+++ src/components/TodosContainer.js
@@ -1,12 +1,18 @@
 import React, { Component } from 'react'
 import update from 'immutability-helper'
 import axios from 'axios'
+import Container from 'react-bootstrap/Container'
+import 'bootstrap/dist/css/bootstrap.min.css'
+import './TodosContainer.css';
+
+
 
 class TodosContainer extends Component {
   constructor(props) {
     super(props)
     this.state = {
-      todos: []
+      todos: [],
+      done_todo: []
     }	
   }
 
@@ -19,7 +25,7 @@ createTodo = (e) => {
       })
     this.setState({
  	 todos: todos,
-  	inputValue: ''
+  	 inputValue: ''
 	})
     })
     .catch(error => console.log(error))      
@@ -46,37 +52,75 @@ handleChange = (e) => {
 }
 
 getTodos() {
-axios.get('/api/v1/todos')
-.then(response => {
-  this.setState({todos: response.data})
-})
-.catch(error => console.log(error))
+    axios.get('/api/v1/todos')
+    .then(response => {
+      this.setState({todos: response.data})
+    })
+    .catch(error => console.log(error))
+    axios.get('/api/v1/done')
+    .then(response => {
+        this.setState({done_todo: response.data})
+    })
 }
 
   componentDidMount() {
     this.getTodos()
   }
 
+   deleteTodo = (id) => {
+    axios.delete(`/api/v1/todos/${id}`)
+    .then(response => {
+      const todoIndex = this.state.todos.findIndex(x => x.id === id)
+      const todos = update(this.state.todos, {
+        $splice: [[todoIndex, 1]]
+      })
+      this.setState({
+        todos: todos
+      })
+    })
+    .catch(error => console.log(error))
+  }
+
   render() {
     return (
-      <div>
-        <div className="inputContainer">
-       		<input className="taskInput" type="text" placeholder="Add a task" maxLength="50" onKeyPress={this.createTodo} value={this.state.inputValue} onChange={this.handleChange} />
-        </div>  	    
-	<div className="listWrapper">
-	   <ul className="taskList">
-		  {this.state.todos.map((todo) => {
-		    return(
-		      <li className="task" todo={todo} key={todo.id}>
-			<input className="taskCheckbox" type="checkbox"  checked={todo.done} onChange={(e) => this.updateTodo(e, todo.id)} />              
-			<label className="taskLabel">{todo.title}</label>
-			<span className="deleteTaskBtn" onClick={(e) => this.deleteTodo(todo.id)}> x</span>
-			 </li>
-		    )       
-		  })} 	    
-	   </ul>
-	</div>
-     </div>
+    <div>
+        <div class="row">
+            <div class="col-md-6">
+                <div class="todolist not-done">
+                 <h1>Todos</h1>
+                    <input className="taskInput" type="text" placeholder="Add a task" maxLength="50" onKeyPress={this.createTodo} value={this.state.inputValue} onChange={this.handleChange} />
+                    <ul id="sortable" class="list-unstyled">
+                        {this.state.todos.map((todo) => {
+                            return(
+                                <li className="task" todo={todo} key={todo.id}>
+                                    <input className="taskCheckbox" type="checkbox"  checked={todo.done} onChange={(e) => this.updateTodo(e, todo.id)} />              
+                                    <label className="taskLabel">{todo.title}</label>
+                                    <span className="deleteTaskBtn" onClick={(e) => this.deleteTodo(todo.id)}> x</span>
+                                </li>
+                            )       
+                         })}   
+                    </ul>
+                    <div class="todo-footer">
+                        <strong><span class="count-todos"></span></strong> Items Left
+                    </div>
+                </div>
+            </div>
+            <div class="col-md-6">
+                <div class="todolist">
+                 <h1>Already Done</h1>
+                    <ul id="done-items" class="list-unstyled">
+                        {this.state.done_todo.map((todo) => {
+                            return(
+                                <li className="task" todo={todo} key={todo.id}>             
+                                    <label className="taskLabel">{todo.title}</label>
+                                </li>
+                            )       
+                         })} 
+                    </ul>
+                </div>
+            </div>
+        </div>
+    </div>
     )
   }
 }
